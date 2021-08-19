69b94fb968e13981b6c8978b24bfcfbfc515a604
Gustavo Dias <gustavoaddias@gmail.com>
2021-08-19 01:47:24 -0300

    Iniciando conexao com o banco de dados

---
+++ rpa-challenge.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+# iniciando conexão com o banco de dados
+try:
+  conn = mysql.connector.connect(host='localhost',database='db_rpa',user='root',password='')
+except mysql.connector.Error as e:
+  print ("Error code:", e.errno)      
+  print ("SQLSTATE value:", e.sqlstate)
+  print ("Error message:", e.msg)     
+  print ("Error:", e)                 
+  s = str(e)
+  print ("Error:", s)     
+
+cursor = conn.cursor()
