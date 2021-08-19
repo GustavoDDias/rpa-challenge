186477a0ee1215441429b635a44a224de62b3986
Gustavo Dias <gustavoaddias@gmail.com>
2021-08-19 02:02:23 -0300

    buscando e inserindo os dados no form

--- rpa-challenge.py
+++ rpa-challenge.py
@@ -27,3 +27,50 @@ with open("docs/excel/contato.csv", encoding='ISO-8859-1') as arquivo:
 
   # ignorar a primeira linha (cabeçalho)
   next(tabela)
+
+  # inserindo os dados nos inputs do formulário
+  for coluna in tabela:
+
+    # nome
+    nome = coluna[0]
+    driver.find_element_by_id("name").send_keys(nome)
+    time.sleep(0.5)
+
+    # email
+    email = coluna[4]
+    driver.find_element_by_id("email").send_keys(email)
+    time.sleep(0.5)
+
+    # cep
+    cep = coluna[3]
+    driver.find_element_by_id("cep").send_keys(cep)
+    time.sleep(0.5)
+
+    # estado
+    estado = coluna[2]
+    driver.find_element_by_id("estado").send_keys(estado)
+    time.sleep(0.5)
+
+    # cidade
+    cidade = coluna[1]
+    driver.find_element_by_id("cidade").send_keys(cidade)
+    time.sleep(0.5)
+
+    # assunto
+    assunto = coluna[6]
+    driver.find_element_by_id("assunto").send_keys(assunto)
+    time.sleep(0.5)
+
+    # telefone 
+    telefone = coluna[5]
+    driver.find_element_by_id("telefone").send_keys(telefone)
+    time.sleep(0.5)
+
+    # mensagem  
+    mensagem = coluna[7] 
+    driver.find_element_by_id("mensagem").send_keys(mensagem)
+    time.sleep(0.5)
+
+    #submit
+    driver.find_element_by_xpath("//input[@type='submit']").click()
+    time.sleep(3)
